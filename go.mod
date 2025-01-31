fe1c3edff6fd67abb80fb8eaca6d284e866f391a
MIS Center <mis@criticalsys.net>
2025-01-31 11:24:07 -0600

    Crypto improvements

--- go.mod
+++ go.mod
@@ -4,12 +4,12 @@ go 1.23.5
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1
-	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/lib/pq v1.10.9
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/microsoft/go-mssqldb v1.8.0
 	github.com/sijms/go-ora/v2 v2.8.23
 	go.mongodb.org/mongo-driver v1.17.2
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -28,5 +28,4 @@ require (
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,194 +1,196 @@
 package main
 
 import (
-        "context"
-        "crypto/aes"
-        "crypto/cipher"
-        "crypto/rand"
-        "database/sql"
-        "encoding/base64"
-        "fmt"
-        "log"
-        "os"
-        "time"
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/tls"
+	"database/sql"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
 
-        "github.com/go-yaml/yaml"
-        _ "github.com/go-sql-driver/mysql"
-        _ "github.com/lib/pq"
-        _ "github.com/mattn/go-sqlite3"
-        "github.com/microsoft/go-mssqldb"
-        "github.com/sijms/go-ora/v2"
-        "go.mongodb.org/mongo-driver/mongo"
-        "go.mongodb.org/mongo-driver/mongo/options"
-        "go.mongodb.org/mongo-driver/bson/primitive"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/microsoft/go-mssqldb"
+	_ "github.com/sijms/go-ora/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/yaml.v3" // Use v3 for better YAML support
 )
 
-type DBConfig struct {
-        Type     string `yaml:"type"`
-        Hostname string `yaml:"hostname"`
-        Port     int    `yaml:"port"`
-        Username string `yaml:"username"`
-        Password string `yaml:"password"`
-        Database string `yaml:"database"`
-        HealthQuery string `yaml:"health_query"`
-        TLS      bool   `yaml:"tls"`
+type DatabaseConfig struct {
+	Type        string `yaml:"type"`
+	Host        string `yaml:"host"`
+	Port        int    `yaml:"port"`
+	User        string `yaml:"user"`
+	Password    string `yaml:"password"`
+	Name        string `yaml:"name"`
+	HealthQuery string `yaml:"health_query"`
+	TLS         bool   `yaml:"tls"`
+}
+
+type Config struct {
+	Databases map[string]DatabaseConfig `yaml:"databases"`
+}
+
+func decrypt(data, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
+
+func xorDecrypt(data []byte, key []byte) []byte {
+	decrypted := make([]byte, len(data))
+	for i := 0; i < len(data); i++ {
+		decrypted[i] = data[i] ^ key[i%len(key)]
+	}
+	return decrypted
+}
+
+func connectAndCheck(config DatabaseConfig, dbID string) error {
+	var db *sql.DB
+	var err error
+
+	decryptedPassword, err := decrypt([]byte(config.Password), []byte(os.Getenv("DB_SECRET_KEY")))
+	if err != nil {
+		return fmt.Errorf("password decryption failed for %s: %w", dbID, err)
+	}
+
+	connectionString := ""
+
+	switch config.Type {
+	case "mysql":
+		tlsConfig := ""
+		if config.TLS {
+			tlsConfig = "?tls=true"
+		}
+		connectionString = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s%s", config.User, string(decryptedPassword), config.Host, config.Port, config.Name, tlsConfig)
+		db, err = sql.Open("mysql", connectionString)
+	case "postgres":
+		tlsConfig := ""
+		if config.TLS {
+			tlsConfig = " sslmode=require"
+		}
+		connectionString = fmt.Sprintf("postgres://%s:%s@%s:%d/%s%s", config.User, string(decryptedPassword), config.Host, config.Port, config.Name, tlsConfig)
+		db, err = sql.Open("postgres", connectionString)
+	case "oracle":
+		connectionString = fmt.Sprintf("%s/%s@%s:%d/%s", config.User, string(decryptedPassword), config.Host, config.Port, config.Name) // Simplified Oracle connection
+		db, err = sql.Open("oracle", connectionString)
+	case "sqlserver":
+		connectionString = fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s", config.User, string(decryptedPassword), config.Host, config.Port, config.Name)
+		db, err = sql.Open("sqlserver", connectionString)
+	case "sqlite":
+		connectionString = config.Name // Database file path
+		db, err = sql.Open("sqlite3", connectionString)
+	case "mongodb":
+		clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%d/%s", config.User, string(decryptedPassword), config.Host, config.Port, config.Name))
+		if config.TLS {
+			clientOptions = clientOptions.SetTLSConfig(&tls.Config{InsecureSkipVerify: true}) // Adjust TLS config as needed
+		}
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		if err != nil {
+			return fmt.Errorf("mongodb connection failed for %s: %w", dbID, err)
+		}
+		defer client.Disconnect(context.TODO())
+
+		err = client.Ping(context.TODO(), nil)
+		if err != nil {
+			return fmt.Errorf("mongodb ping failed for %s: %w", dbID, err)
+		}
+
+		db := client.Database(config.Name)
+
+		names, err := db.ListCollectionNames(context.TODO(), primitive.M{})
+		if err != nil {
+			return fmt.Errorf("mongodb list collections failed for %s: %w", dbID, err)
+		}
+		fmt.Println("Collections found:", names)
+
+		return nil // MongoDB check successful
+	default:
+		return fmt.Errorf("unsupported database type: %s", config.Type)
+	}
+
+	if err != nil {
+		return fmt.Errorf("connection failed for %s: %w", dbID, err)
+	}
+	defer db.Close()
+
+	if config.Type != "mongodb" { // Skip health check for MongoDB (handled above)
+		if _, err := db.Exec(config.HealthQuery); err != nil {
+			return fmt.Errorf("health check query failed for %s: %w", dbID, err)
+		}
+	}
+
+	fmt.Printf("Successfully connected and checked %s\n", dbID)
+	return nil
 }
 
 func main() {
-        // Load configuration from YAML file
-        configFile, err := os.ReadFile("dbconfig.yaml")
-        if err != nil {
-                log.Fatal("Error reading config file:", err)
-        }
+	configFile := flag.String("config", "config.yaml", "Path to the configuration file")
+	dbID := flag.String("db", "", "Identifier of the database to check")
+	flag.Parse()
 
-        var configs map[string]DBConfig
-        err = yaml.Unmarshal(configFile, &configs)
-        if err != nil {
-                log.Fatal("Error unmarshalling config:", err)
-        }
+	if os.Getenv("DB_SECRET_KEY") == "" {
+		fmt.Println("DB_SECRET_KEY environment variable is not set.")
+		os.Exit(1)
+	}
 
-        // Get and decrypt the secret key
-        secretKeyObfuscated := os.Getenv("DB_SECRET_KEY")
-    if secretKeyObfuscated == "" {
-        log.Fatal("DB_SECRET_KEY environment variable not set")
-    }
+	// XOR decrypt the secret key
+	key := []byte(os.Getenv("DB_SECRET_KEY"))
 
-    secretKey, err := decryptSecretKey(secretKeyObfuscated)
-    if err != nil {
-        log.Fatal("Error decrypting secret key:", err)
-    }
+	// Example obfuscated key (replace with your actual obfuscated key)
+	obfuscatedKey, _ := base64.StdEncoding.DecodeString("your_obfuscated_key_here")
 
-    // Use the deobfuscated key for decryption later on
-    // ...
+	deobfuscatedKey := xorDecrypt(obfuscatedKey, key)
+	os.Setenv("DB_SECRET_KEY", string(deobfuscatedKey))
 
-        for name, config := range configs {
-                fmt.Printf("Checking connection for %s...\n", name)
+	data, err := os.ReadFile(*configFile)
+	if err != nil {
+		fmt.Printf("Error reading config file: %v\n", err)
+		os.Exit(1)
+	}
 
-                // Decrypt password if needed.
-                if config.Password != "" {
-                    config.Password, err = decrypt(config.Password, secretKey)
-                    if err != nil {
-                        log.Fatalf("Error decrypting password for %s: %v", name, err)
-                    }
-                }
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Printf("Error unmarshalling config file: %v\n", err)
+		os.Exit(1)
+	}
 
-                err = checkConnection(config)
-                if err != nil {
-                        fmt.Printf("Connection check for %s failed: %v\n", name, err)
-                } else {
-                        fmt.Printf("Connection check for %s successful.\n", name)
-                }
-        }
-}
-
-func decryptSecretKey(obfuscated string) ([]byte, error) {
-    // XOR deobfuscation (example, adapt as needed)
-    key := []byte("your_xor_key") // Replace with your XOR key
-    decrypted := make([]byte, len(obfuscated))
-    for i := 0; i < len(obfuscated); i++ {
-        decrypted[i] = obfuscated[i] ^ key[i%len(key)]
-    }
-    return decrypted, nil
-}
-
-func decrypt(encrypted string, key []byte) (string, error) {
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return "", err
-    }
-    gcm, err := cipher.NewGCM(block)
-    if err != nil {
-        return "", err
-    }
-    nonceSize := gcm.NonceSize()
-    nonce, ciphertext := encrypted[:nonceSize], encrypted[nonceSize:]
-    plaintext, err := gcm.Open(nil, []byte(nonce), []byte(ciphertext), nil)
-    if err != nil {
-        return "", err
-    }
-    return string(plaintext), nil
-}
-
-
-
-func checkConnection(config DBConfig) error {
-        var db *sql.DB
-        var err error
-
-        switch config.Type {
-        case "mysql":
-                        var tlsConfig string
-                        if config.TLS {
-                                tlsConfig = "&tls=true"
-                        }
-                        connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true%s", config.Username, config.Password, config.Hostname, config.Port, config.Database, tlsConfig)
-                        db, err = sql.Open("mysql", connectionString)
-        case "postgres":
-                        var tlsConfig string
-                        if config.TLS {
-                                tlsConfig = "sslmode=require" // Or verify-full if you have certs
-                        }
-                        connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?%s", config.Username, config.Password, config.Hostname, config.Port, config.Database, tlsConfig)
-                        db, err = sql.Open("postgres", connectionString)
-        case "oracle":
-                        var tlsConfig string
-            if config.TLS {
-                tlsConfig = "?connection_string=TRUE&server_cert_mode=1"
-            }
-                        connectionString := fmt.Sprintf("%s/%s@%s:%d/%s%s", config.Username, config.Password, config.Hostname, config.Port, config.Database, tlsConfig)
-                        db, err = sql.Open("oracle", connectionString)
-        case "sqlserver":
-                        var tlsConfig string
-            if config.TLS {
-                tlsConfig = "encrypt=true"
-            }
-                        connectionString := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&%s", config.Username, config.Password, config.Hostname, config.Port, config.Database, tlsConfig)
-                        db, err = sql.Open("sqlserver", connectionString)
-        case "sqlite":
-                        connectionString := config.Database // SQLite database file path
-                        db, err = sql.Open("sqlite3", connectionString)
-        case "mongodb":
-                        var tlsConfig string
-                        if config.TLS {
-                                tlsConfig = "&tls=true"
-                        }
-                        connectionURI := fmt.Sprintf("mongodb://%s:%s@%s:%d/%s%s", config.Username, config.Password, config.Hostname, config.Port, config.Database, tlsConfig)
-                        clientOptions := options.Client().ApplyURI(connectionURI)
-
-                        client, err := mongo.Connect(context.Background(), clientOptions)
-                        if err != nil {
-                                return err
-                        }
-
-                        err = client.Ping(context.Background(), nil)
-                        if err != nil {
-                                return err
-                        }
-
-                        // List collections (mongodb healthcheck)
-                        names, err := client.Database(config.Database).ListCollectionNames(context.Background(), primitive.M{})
-                        if err != nil {
-                                return err
-                        }
-
-                        fmt.Println("Collections in", config.Database, ":", names)
-            return nil // MongoDB connection and healthcheck successful
-        default:
-                return fmt.Errorf("unsupported database type: %s", config.Type)
-        }
-
-        if err != nil {
-                return err
-        }
-        defer db.Close()
-
-
-        if config.Type != "mongodb" && config.HealthQuery != "" { // Skip health query for MongoDB
-                _, err = db.Exec(config.HealthQuery)
-                if err != nil {
-                        return fmt.Errorf("health query failed: %w", err)
-                }
-        }
-
-        return nil
+	if *dbID != "" {
+		dbConfig, ok := config.Databases[*dbID]
+		if !ok {
+			fmt.Printf("Database with ID '%s' not found in config\n", *dbID)
+			os.Exit(1)
+		}
+		if err := connectAndCheck(dbConfig, *dbID); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		for id, dbConfig := range config.Databases {
+			if err := connectAndCheck(dbConfig, id); err != nil {
+				fmt.Println(err)
+				// Decide whether to continue on error or exit.  Currently continues.
+				// os.Exit(1)  // Uncomment to stop on first error.
+			}
+		}
+	}
 }
